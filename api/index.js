aade85437b1ce846dae39a4c267a169108d79387
Mag091986 <k9921star@gmail.com>
2024-09-10 23:03:00 -0500

    Por fin conecté la DB a mongo en Cloud Atlas

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('¡Conectado a MongoDB!');
+})
+.catch((err) => {
+    console.log(err);
+})
 
 const app = express();
 
 app.listen(3000, () => {
-console.log('Servidor está corriendo en puerto 3000');
+    console.log('Servidor está corriendo en puerto 3000');
 });
