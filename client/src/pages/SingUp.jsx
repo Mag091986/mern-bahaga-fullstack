f31fb6df422fe76233769c6c5bb92ef33c1ba985
Mag091986 <k9921star@gmail.com>
2024-09-15 11:39:51 -0500

    Ajustes Login

--- client/src/pages/SingUp.jsx
+++ client/src/pages/SingUp.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <div>
       <h1 className='text-3xl text-center font-semibold my-7'>Registro</h1>
