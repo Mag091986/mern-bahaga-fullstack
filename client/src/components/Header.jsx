10a04da0eb981e55a4b76972bf8c7c34ba9b73dd
Mag091986 <k9921star@gmail.com>
2024-09-21 20:19:21 -0500

    Ajustes Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Header() {
   return (
     <header className='bg-orange-600 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        {/* Logo Section */}
+        {/* Seccion del Logo */}
         <Link to='/' className='flex justify-between items-center'>
           <img src={logo} alt='Logo' className='w-10 mr-4' />
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
@@ -23,7 +23,7 @@ export default function Header() {
           </h1>
         </Link>
 
-        {/* Search Form */}
+        {/* Barra de busqueda */}
         <form className='bg-orange-100 p-2 rounded-lg flex items-center'>
           <input
             type='text'
@@ -33,12 +33,12 @@ export default function Header() {
           <FaSearch className='text-gray-500' />
         </form>
 
-        {/* Hamburger Icon for Mobile */}
+        {/* Icono hamburguesa para mobile */}
         <button className='sm:hidden text-white' onClick={toggleMenu}>
           <FaBars className='w-6 h-6' />
         </button>
 
-        {/* Navigation Links for Desktop */}
+        {/* Version de esritorio */}
         <ul className='hidden sm:flex gap-4 items-center'>
           <Link to='/'><li className='text-white hover:underline'>Inicio</li></Link>
           <Link to='/about'><li className='text-white hover:underline'>Nosotros</li></Link>
@@ -48,7 +48,7 @@ export default function Header() {
                 <img
                   src={currentUser.avatar}
                   alt= 'Perfil de usuario'
-                  className='w-8 h-8 rounded-full' // Added some styling for size and shape
+                  className='w-8 h-8 rounded-full' //Estilo de avatar redondo
                 />
               </li>
             ) : (
@@ -58,7 +58,7 @@ export default function Header() {
         </ul>
       </div>
 
-      {/* Mobile Menu (visible when toggled) */}
+      {/*Menu para mobile (visible when toggled) */}
       {isOpen && (
         <ul className='flex flex-col bg-orange-600 p-4 sm:hidden'>
           <Link to='/' onClick={toggleMenu}>
@@ -73,7 +73,7 @@ export default function Header() {
                 <img
                   src={currentUser.avatar}
                   alt= 'Perfil de usuario'
-                  className='w-8 h-8 rounded-full' // Consistent styling for mobile view
+                  className='w-8 h-8 rounded-full' // Version para mobile
                 />
                 <span className='ml-2 text-white'>{currentUser.name}</span>
               </li>
