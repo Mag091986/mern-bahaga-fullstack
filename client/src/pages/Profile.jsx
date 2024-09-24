ef9a16cb05c4814453a46cf69c48bbfa5665eef7
Mag091986 <k9921star@gmail.com>
2024-09-23 23:33:59 -0500

    Hay un error con el direccionamienro del backend

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,17 @@
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { app } from '../firebase';
+import { updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice';
 
 export default function Profile() {
   const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
-  const [formData, setFormData] = useState({ avatar: '' });
+  const [formData, setFormData] = useState({});
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (file) {
@@ -23,28 +25,57 @@ export default function Profile() {
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on('state_changed',
+    uploadTask.on(
+      'state_changed',
       (snapshot) => {
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        setFilePerc(Math.round(progress)); // Update file upload percentage
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
       },
       (error) => {
-        console.error('Error durante la carga:', error); // Handle upload error
+        setFileUploadError(true);
       },
       () => {
-        // On successful upload, get the download URL
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setFormData({ ...formData, avatar: downloadURL }); // Update form data with avatar URL
-          console.log('File available at:', downloadURL); // Log the download URL
-        });
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
       }
     );
-  }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Perfil de Usuario</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input onChange={(e) => setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*' />
         <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="Foto de perfil" className='rounded-full h-20 w-20 object-cover cursor-pointer self-center mt-2' />
         <p className='text-sm self-center'>
@@ -52,17 +83,17 @@ export default function Profile() {
             <span className='text-red-600'>
               Error de carga (El archivo debe pesar menos de 2 mb)
             </span>) :
-                filePerc > 0 && filePerc < 100 ? (
-          <span className='text-slate-600'> 
-            {`Carga al ${filePerc} %`}</span>
-          ) :
-          filePerc === 100 ? (
-          <span className='text-green-600'>¡Carga completa!</span>
-  ) : ('')}
+            filePerc > 0 && filePerc < 100 ? (
+              <span className='text-slate-600'>
+                {`Carga al ${filePerc} %`}</span>
+            ) :
+              filePerc === 100 ? (
+                <span className='text-green-600'>¡Carga completa!</span>
+              ) : ('')}
         </p>
-        <input type="text" placeholder="Nombre de Usuario" id="username" className="border p-3 rounded-lg" />
-        <input type="email" placeholder="Correo electrónico" id="email" className="border p-3 rounded-lg" />
-        <input type="text" placeholder="Contraseña" id="password" className="border p-3 rounded-lg" />
+        <input type="text" placeholder="Nombre de Usuario" defaultValue={currentUser.username} id="username" className="border p-3 rounded-lg" onChange={handleChange} />
+        <input type="email" placeholder="Correo electrónico" defaultValue={currentUser.email} id="email" className="border p-3 rounded-lg" onChange={handleChange} />
+        <input type="text" placeholder="Contraseña" id="password" className="border p-3 rounded-lg" onChange={handleChange} />
         <button className="bg-sky-500 border uppercase text-white p-3 rounded-lg hover:bg-orange-600 disabled:opacity-50">Actualizar</button>
       </form>
       <div className="flex justify-between mt-4">
