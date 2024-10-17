f0077b08fcee977686f56b5d52a773bdada6b367
Mag091986 <k9921star@gmail.com>
2024-10-16 23:21:14 -0500

    Completamos la funcionalidad y el front del listado de Salones en el Usuario.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,13 +12,14 @@ export default function Profile() {
   const [formData, setFormData] = useState({});
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
-  const [updateSuccess, setUpdateSuccess] = useState(false)
-
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (file) {
-      handleFileUpload( );
+      handleFileUpload();
     }
   }, [file]);
 
@@ -97,7 +98,7 @@ export default function Profile() {
       dispatch(signOutUserStart());
       const res = await fetch('/api/auth/signout');
       const data = await res.json();
-  
+
       if (data.success === false) {
         dispatch(signOutUserFailure(data.message));
         return;
@@ -108,7 +109,22 @@ export default function Profile() {
       dispatch(signOutUserFailure(error.message));
     }
   };
-  
+
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -141,6 +157,26 @@ export default function Profile() {
       </div>
       <p className='text-red-700 mt-5' > {error ? error : ''} </p>
       <p className='text-green-600 mt-5'> {updateSuccess ? 'Actualización Completa' : ''} </p>
+      <button onClick={handleShowListings} className='text-green-700 w-full'>Mostrar salones</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error al mostrar los salones' : ''}</p>
+      {userListings && userListings.length > 0 &&
+      <div className='flex flex-col gap-4'>
+<h1 className='text-center mt-7 text-2xl font-semibold'>Tus Salones</h1>
+        {userListings.map((listing) =>
+          <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+            <Link to={`/listing/${listing._id}`}>
+              <img src={listing.imageUrls[0]} alt='listing cover' className='h-16 w-16 object-contain' />
+            </Link>
+            <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
+            <p>{listing.name}</p>
+            </Link>
+            <div className='flex flex-col items-center'>
+  <button className='text-red-700 uppercase'>Borrar</button>
+  <button className='text-green-700 uppercase'>Editar</button>
+            </div>
+          </div>
+        )}
+      </div>}
     </div>
   )
 }
