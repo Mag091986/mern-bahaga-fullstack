46c5c3005e58219285a165e2f489ac9c7478e647
Mag091986 <k9921star@gmail.com>
2024-10-24 22:16:39 -0500

    Creamos la funcionalidad para Actualizar los Salones.

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,6 @@
+import mongoose from 'mongoose';
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
-
 
 export const createListing = async (req, res, next) => {
     try {
@@ -12,20 +12,57 @@ export const createListing = async (req, res, next) => {
 };
 
 export const deleteListing = async (req, res, next) => {
-    const listing = await Listing.findById(req.params.id);
+    const { id } = req.params;
+
+    // Validate ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return next(errorHandler(400, 'ID inválido'));
+    }
+
+    const listing = await Listing.findById(id);
 
     if (!listing) {
         return next(errorHandler(404, '¡Salón no encontrado!'));
     }
 
     if (req.user.id !== listing.userRef) {
-        return next(errorHandler(401, '¡Solamente puedes eliminar tus propios salones!'))
+        return next(errorHandler(401, '¡Solamente puedes eliminar tus propios salones!'));
     }
 
     try {
-        await Listing.findByIdAndDelete(req.params.id);
-        res.status(200).json("El salón ha sudo eliminado");
+        await Listing.findByIdAndDelete(id);
+        res.status(200).json("El salón ha sido eliminado");
     } catch (error) {
         next(error);
+    }
+};
+
+export const updateListing = async (req, res, next) => {
+    const { id } = req.params;
+
+    // Validate ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return next(errorHandler(400, 'ID inválido'));
+    }
+
+    const listing = await Listing.findById(id);
+
+    if (!listing) {
+        return next(errorHandler(404, "¡Salón no encontrado!"));
+    }
+
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, "¡Solamente puedes editar tus salones!"));
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);  
     }
 };
