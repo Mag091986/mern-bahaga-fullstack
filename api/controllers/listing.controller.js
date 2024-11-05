d1fd71443fd5fef2828c1962e3e687e222c90975
Mag091986 <k9921star@gmail.com>
2024-11-04 23:32:43 -0500

    Creamos el api de la busqueda

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -71,9 +71,69 @@ export const getListing = async (req, res, next) => {
     try {
         const listing = await Listing.findById(req.params.id);
         if (!listing) {
-return next(errorHandler(404, "¡Salón no encontrado!"));
+            return next(errorHandler(404, "¡Salón no encontrado!"));
         }
         res.status(200).json(listing);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getListings = async (req, res, next) => {
+    try {
+
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let offer = req.query.offer;
+
+        if (offer === undefined || offer === 'false') {
+            offer = { $in: [false, true] };
+        }
+
+        let decoration = req.query.decoration;
+
+        if (decoration === undefined || decoration === 'false') {
+            decoration = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] };
+        }
+
+        let catering = req.query.catering;
+
+        if (catering === undefined || catering === 'false') {
+            catering = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+
+        if (type === undefined || type === 'all') {
+            type = { $in: ['day', 'hour'] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+        
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: { $regex: searchTerm, $options: 'i'},
+            offer,
+            decoration,
+            parking,
+            catering,
+            type,
+        }).sort(
+            {[sort]: order}
+        ).limit(limit).skip(startIndex);
+
+        return res.status(200).json(listings);
+
     } catch (error) {
         next(error);
     }
